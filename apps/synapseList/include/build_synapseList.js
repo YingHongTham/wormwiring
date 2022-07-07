b8a5ca06efb39e525ab9e5696b44e116579fb695
yinghong <thamyinghong@gmail.com>
2022-07-07 00:17:16 -0400

    good working version, added buttons to expand/collapse individual synapse/summary rows

--- apps/synapseList/include/build_synapseList.js
+++ apps/synapseList/include/build_synapseList.js
@@ -28,6 +28,53 @@ window.onload = function()
     }
   }
 
+
+  //====================================
+  // functionality of buttons
+
+  function toggleAllIndividualRows(expanded) {
+    const btnIndv = document.getElementById('toggle-all-individual');
+    // set value, text accordingly
+    btnIndv.value = expanded ? 'on' : 'off';
+    btnIndv.innerHTML = expanded ?
+        'Hide All Individual Synapses' :
+        'Show All Individual Synapses';
+    const synRows = document.querySelectorAll('.individual');
+    synRows.forEach( row => {
+      row.classList.toggle('in', expanded);
+      row.setAttribute('aria-expanded', expanded);
+    });
+  }
+  const btnIndv = document.getElementById('toggle-all-individual');
+  btnIndv.onclick = () => {
+    // new state, true if expanded, false if collapsed
+    const expanded = btnIndv.value !== 'on';
+    toggleAllIndividualRows(expanded);
+  };
+  btnIndv.click(); // so that expanded by default
+
+
+  function toggleAllSummaryRows(expanded) {
+    const btnSumm = document.getElementById('toggle-all-summary');
+    // set value, text accordingly
+    btnSumm.value = expanded ? 'on' : 'off';
+    btnSumm.innerHTML = expanded ?
+        'Hide All Summary Rows' :
+        'Show All Summary Rows';
+    const summRows = document.querySelectorAll('.summary');
+    summRows.forEach( row => {
+      row.classList.toggle('collapse', !expanded);
+    });
+  }
+
+  const btnSumm = document.getElementById('toggle-all-summary');
+  btnSumm.onclick = () => {
+    // new state, true if expanded, false if collapsed
+    const expanded = btnSumm.value !== 'on';
+    toggleAllSummaryRows(expanded);
+  };
+
+  //=============================================
   // set links to Partner List app
   // (by default it's set to partnerList with no url params)
   const urlPartnerList = '../partnerList/'
@@ -36,10 +83,13 @@ window.onload = function()
   const pnava = document.getElementById('nav-partner-list');
   pnava.href = urlPartnerList;
   
+  //================================================
   // set Cell Name in the HTML
   const cellElem = document.getElementById('cell-name');
   cellElem.innerHTML = `Cell Name: ${cell}`;
 
+  //======================================================
+  // requesting for data
   // url to php which makes the MySQL queries
   const url = '../php/getSynapseList-alt.php/'
     + `?db=${db}`
@@ -66,88 +116,99 @@ window.onload = function()
           }
           synByPartner[syn.partner].summary.count += 1;
           synByPartner[syn.partner].summary.sections += syn.sections;
-          synByPartner[syn.partner].push(syn);
+          synByPartner[syn.partner].synList.push(syn);
         }
 
-		    //visible list element
-		    for (var i in data[type].list) {
-		      var tbody1 = document.createElement('tbody');
-		      //tbody.className = 'labels';
-		      var tr = document.createElement('tr');
-		      tr.className = 'labels';
-		      for (var j  in data[type].list[i]) {
-			      var td = document.createElement('td');
-			      if (j == 0){
-			        td.colSpan = 4;
-			        td.class = 'rcol';
-			        var label = document.createElement('label');
-			        label.setAttribute('for',data[type].list[i][j]);
-			        label.innerHTML = data[type].list[i][j];
-			        var input = document.createElement('input');
-			        input.name = data[type].list[i][j];
-			        input.type = 'checkbox';
-			        input.id = data[type].list[i][j];
-			        input.setAttribute('data-toggle','toggle');
-			        input.onclick = function(){
-				        $(this).parents().next('.hide').toggle();
-			        };
-			      td.appendChild(label);
-			      td.appendChild(input);
-			      } else {
-			        td.colsSpan = 1;
-			        td.class = 'lcol';
-			        td.innerHTML = data[type].list[i][j];
-			      };
-			      tr.appendChild(td);
-		      };
-		      tbody1.appendChild(tr)
-		      tbl.appendChild(tbody1);
+        // there are two types of rows:
+        // -summary of synapses with given partner
+        // -individual synapse
+        // kept track globally with addition of classes
+        // 'summary' and 'individual'
+        //
+        // they are organized to have the summary row
+        // followed by synapses with that partner
+        // and they would have a class distinguishing them
+        // from other partners/synapse type:
+        // `group-${synType}-${partner}`
+        //
+        // so for example, if I want to target the rows
+        // corresponding to individual synapses
+        // which has the partner 'AVDR' of 'gap' type,
+        // I would query for elements with classes
+        // '.individual .group-gap-AVDR'
+        //
+        // in particular, we use bootstrap, data-toggle,
+        // to target rows to collapse/show
+        for (const partner in synByPartner) {
+          const groupClassName = `group-${type}-${partner}`;
 
-		      //hidden list element
-		      var tbody2 = document.createElement('tbody');
-		      tbody2.className = 'hide';
-		      tbody2.setAttribute('style','display:none;');
-		      for (var j in data[type].synList[i]) {
-			      var tr = document.createElement('tr');
-			      var td = document.createElement('td');
-			      td.className = 'rcol'
-			      tr.appendChild(td);
-			      var td = document.createElement('td');
-			      td.className = 'lcol'
-			      td.innerHTML = data[type].synList[i][j][1];
-			      tr.appendChild(td);
-			      var td = document.createElement('td');
-			      td.className = 'lcol'
-			      //td.innerHTML = data[type].synList[i][j][3];
-			      var a = document.createElement('a');
-			      var href = '../synapseViewer/?neuron=' + 
-			        self.synapses.continName + '&db=' + 
-			        data[type].synList[i][j][1] + '&continNum=' +
-			        data[type].synList[i][j][3] + '&series=' +
-			        self.synapses.series;
-			      a.href = href;
-			      a.innerHTML = data[type].synList[i][j][3];
-			      td.appendChild(a);
-			      tr.appendChild(td);			
-			      var td = document.createElement('td');
-			      td.className = 'lcol'
-			      td.innerHTML = data[type].synList[i][j][4];
-			      tr.appendChild(td);			
-			      var td = document.createElement('td');
-			      td.className = 'lcol'
-			      tr.appendChild(td);
-			      var td = document.createElement('td');
-			      td.className = 'lcol'
-			      td.innerHTML = data[type].synList[i][j][2];
-			      tr.appendChild(td);
-			      tbody2.appendChild(tr)
-			      tbl.appendChild(tbody2);
-		      }
-		    }
+          // row showing summary for given partner
+          const trSummary = document.createElement('tr');
+          const tdPartner = document.createElement('td');
+          const tdCount = document.createElement('td');
+          const tdSections = document.createElement('td');
+          
+          tbl.appendChild(trSummary);
+          trSummary.appendChild(tdPartner);
+          trSummary.appendChild(tdCount);
+          trSummary.appendChild(tdSections);
+
+          trSummary.classList.add(groupClassName);
+          trSummary.classList.add('summary');
+          // clicking this row toggles the class 'collapse'
+          // among all elements matching 'data-target'
+          // see also in index.html
+          trSummary.setAttribute('data-toggle','collapse');
+          trSummary.setAttribute('data-target',
+              `.${groupClassName}.individual`);
+          // need 'role' attribute because trSummary is not button
+          trSummary.setAttribute('role', 'button');
+
+          const summary = synByPartner[partner].summary;
+
+          tdPartner.innerHTML = partner;
+          tdPartner.colSpan = 4;
+          tdCount.innerHTML = summary.count;
+          tdCount.colSpan = 1;
+          tdSections.innerHTML = summary.sections;
+          tdSections.colSpan = 1;
+
+          // row for each individual synapse
+          const synList = synByPartner[partner].synList;
+          for (const syn of synList) {
+            const trIndiv = document.createElement('tr');
+            const tdPartner = document.createElement('td');
+            const tdDatabase = document.createElement('td');
+            const tdContin = document.createElement('td');
+            const tdZ = document.createElement('td');
+            const tdCount = document.createElement('td');
+            const tdSections = document.createElement('td');
+
+            tbl.appendChild(trIndiv);
+            trIndiv.appendChild(tdPartner);
+            trIndiv.appendChild(tdDatabase);
+            trIndiv.appendChild(tdContin);
+            trIndiv.appendChild(tdZ);
+            trIndiv.appendChild(tdCount);
+            trIndiv.appendChild(tdSections);
+
+            trIndiv.classList.add(groupClassName);
+            trIndiv.classList.add('individual');
+            trIndiv.classList.add('collapse'); // data-toggle
+            //trIndiv.classList.add('show'); messes things up
+
+            tdPartner.innerHTML = partner;
+            tdDatabase.innerHTML = db;
+            tdContin.innerHTML = syn.contin;
+            tdZ.innerHTML = syn.z;
+            tdSections.innerHTML = syn.sections;
+          }
+        }
 	    }
+
+      toggleAllIndividualRows(true);
 	  }
   };
   xhttp.open("GET",url,true);
   xhttp.send();
-
 }
